b631ad01d81090f1329021fb731222762ce7d5e8
Naga Sai <108686878+saijanipireddy@users.noreply.github.com>
2025-02-03 10:56:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+
+dotenv.config();  
 
 export const connectDB = async () => {
-     mongoose.connect("mongodb://localhost:27017/food-del")
+     mongoose.connect(process.env.MONGO_URI)
     .then(()=> console.log('Db Connected Succesfully'))
     .catch((err) => console.log('Db Connection Error'));
 
