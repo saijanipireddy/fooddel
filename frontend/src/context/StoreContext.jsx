048923690f1f5a4763cfa1c85c9e73d7a2a2fd7d
Naga Sai <108686878+saijanipireddy@users.noreply.github.com>
2025-02-03 09:27:35 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("");
-    const url = "http://localhost:4000";
+    const url = "https://fooddel-backend-4d76.onrender.com/";
     const [food_list, setFoodList] = useState([]);
 
     // Load cart items from localStorage when the component mounts
