3c370c15ee0e38b340cb4340b53604384c8abd04
Naga Sai <108686878+saijanipireddy@users.noreply.github.com>
2025-02-03 13:34:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 4000
 // middleware 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: 'https://fooddel-frontend-50kw.onrender.com/' }))
 
 // DB Connection 
 connectDB();
